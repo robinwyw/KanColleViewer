fdc9c9d38a1876369021447841c23188c5b3ab63
robinwyw <wuweilogo@live.com>
2025-10-21 19:07:53 +0800

    update version to 4.5.3

--- source/Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
+++ source/Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Windows;
 	ResourceDictionaryLocation.None,
 	ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("4.5.2.121")]
+[assembly: AssemblyVersion("4.5.3")]
